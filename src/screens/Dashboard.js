685cc8f1f84e3e49f57a022da6524f6ccf9c4121
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-06-29 23:19:15 +0530

    dashboard 0.1

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import {View, Text,StyleSheet,TouchableOpacity} from 'react-native';
 
-import {Appbar} from 'react-native-paper';
+import {Appbar,Card,Avatar} from 'react-native-paper';
 
 export default class Dashboard extends React.Component{
 
@@ -10,11 +10,33 @@ export default class Dashboard extends React.Component{
             <View style={StyleSheet.container}>
                 <Appbar.Header style={{backgroundColor:'purple'}}>
                         <Appbar.Content 
-                        title="Dashboard"
+                        title="DASHBOARD"
                         style={{alignItems: 'center',justifyContent: 'center'}}
                         />
+                        <Appbar.Action icon="menu" />
                 </Appbar.Header>
-                <Text>Dashboard</Text>
+                <View style={{alignItems: 'center',justifyContent: 'center',marginTop:50}}>
+                   <View style={{flexDirection:'row'}}>
+                       <TouchableOpacity>
+                       <Card style={styles.cardContainer}><Avatar.Icon size={40} icon="ticket" style={{backgroundColor:'purple'}} /></Card>
+                       </TouchableOpacity>
+                        
+                       <TouchableOpacity>
+                       <Card style={styles.cardContainer}><Avatar.Icon size={40} icon="map" style={{backgroundColor:'purple'}}/></Card>
+                       </TouchableOpacity>
+                    </View>
+
+                    <View style={{flexDirection:'row'}}>
+                    <TouchableOpacity>
+                       <Card style={styles.cardContainer}><Avatar.Icon size={40} icon="camera"  style={{backgroundColor:'purple'}}/></Card>
+                       </TouchableOpacity>
+                       <TouchableOpacity>
+                       <Card style={styles.cardContainer}><Avatar.Icon size={40} icon="folder" style={{backgroundColor:'purple'}} /></Card>
+                       </TouchableOpacity>
+                    </View>
+
+                </View>
+            
             </View>
         );
     }
@@ -24,5 +46,21 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+     },
+     cardContainer:{
+         shadowColor: 'black',
+         shadowOffset : {width: 0, height:2},
+         shadowRadius: 6,
+         shadowOpacity: 0.30,
+         elevation: 10,
+         padding: 20,
+         borderRadius: 10,
+         justifyContent: 'center',
+         alignItems: 'center',
+         width:120,
+         height:120,
+         marginTop:20,
+         marginHorizontal:20,
+         marginVertical:10
      }
 });
