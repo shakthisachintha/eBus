3b48898d96a3933cfed912c5b779d6e88687f31c
Shakthi Sachintha <shakthisachintha@gmail.com>
2020-07-02 19:55:56 +0530

    facebook login implemented

--- src/screens/Authentication/LoginScreen.js
+++ src/screens/Authentication/LoginScreen.js
@@ -2,15 +2,78 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground, Modal } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import { CommonActions } from '@react-navigation/native';
+import { LoginManager, LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+
 
 import colors from '../../utils/colors';
 import images from '../../utils/images';
 
+// Somewhere in your code
+signIn = async () => {
+
+};
+
 export default class LoginScreen extends React.Component {
 
-  state = {
-    addPasswordVisible: false
-  };
+
+  constructor(props) {
+    super();
+    this.state = {
+      isLogged: false,
+      name: null,
+      email: null,
+      image: null,
+    };
+    this.loginWithFacebookAlt = this.loginWithFacebookAlt.bind(this);
+  }
+
+
+  loginWithFacebookAlt(that = this) {
+    // Attempt a login using the Facebook login dialog asking for default permissions.
+    LoginManager.logInWithPermissions(["public_profile", "email"]).then(
+      function (result) {
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          console.log("Login Success", result.grantedPermissions.toString())
+          AccessToken.getCurrentAccessToken().then((accessToken) => {
+            const request = new GraphRequest('/me', {
+              accessToken: accessToken.accessToken, parameters: { fields: { string: 'id, name, email,picture.type(large)' } }, httpMethod: "POST"
+            }, function (error: ?Object, result: ?Object) {
+              if (error) {
+                console.log('Error fetching data: ', error.toString());
+              } else {
+                console.log('Success fetching');
+                that.setState({ isLogged: true });
+                that.setState({ email: result.email });
+                that.setState({ image: result.picture.data.url });
+                that.setState({ name: result.name });
+                console.log(result);
+              }
+            });
+            new GraphRequestManager().addRequest(request).start();
+          })
+        }
+      },
+      function (error) {
+        console.log("Login fail with error: " + error);
+      }
+    );
+  }
+
+  loginWithFacebook(accessToken) {
+    const request = new GraphRequest('/me', {
+      accessToken, parameters: { fields: { string: 'id, name, email, first_name, last_name, gender,picture' } }, httpMethod: "POST"
+    }, function (error: ?Object, result: ?Object) {
+      if (error) {
+        console.log('Error fetching data: ', error.toString());
+      } else {
+        console.log('Success fetching data: ', result);
+      }
+    });
+
+    new GraphRequestManager().addRequest(request).start();
+  }
 
   render({ navigation } = this.props) {
 
@@ -29,12 +92,34 @@ export default class LoginScreen extends React.Component {
       <View style={StyleSheet.container}>
 
         <ImageBackground source={images.LOGING_BACKGROUND} style={styles.backgroundImage} >
+          <LoginButton permissions={["public_profile", "email", "pages_show_list"]}
+            onLoginFinished={
+              (error, result) => {
+                if (error) {
+                  console.log("login has error: " + error);
+                } else if (result.isCancelled) {
+                  console.log("login is cancelled.");
+                } else {
+                  console.log(result);
+                  AccessToken.getCurrentAccessToken().then(
+                    (data) => {
+                      console.log(data);
+                      console.log(data.accessToken.toString())
+                      this.loginWithFacebook(data.accessToken);
+                    }
+                  )
+                }
+              }
+            }
+            onLogoutFinished={() => console.log("logout.")} />
+
 
           <Image
             style={styles.topImage}
             source={images.LOGO}
             resizeMode="contain"
           />
+
           <Text style={styles.headText}>Login</Text>
 
           <View style={{ flexDirection: 'column', marginTop: 0 }}>
@@ -59,14 +144,14 @@ export default class LoginScreen extends React.Component {
 
           <Text style={styles.textSocial}>or Via Social Media</Text>
           <View style={styles.icon}>
-            <TouchableOpacity style={styles.image}>
+            <TouchableOpacity onPress={() => this.loginWithFacebookAlt()} style={styles.image}>
               <Image
                 style={styles.socialIcon}
                 source={images.FACEBOOK_ICON}
                 resizeMode="contain"
               />
             </TouchableOpacity>
-            <TouchableOpacity style={styles.image}>
+            <TouchableOpacity onPress={() => signIn()} style={styles.image}>
               <Image
                 style={styles.image}
                 source={images.GOOGLE_ICON}
