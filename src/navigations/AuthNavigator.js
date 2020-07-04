372c177c99bea6db3c18947f04c109e4af3db54e
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-07-04 14:26:19 +0530

    Update AuthNavigator.js

--- src/navigations/AuthNavigator.js
+++ src/navigations/AuthNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 
 import DashboardNavigator from './DashboardNavigator';
 import LoginScreen from "../screens/Authentication/LoginScreen";
-import LoadingScreen from '../screens/LoadingScreen';
+import LoadingScreen from '../screens/loadingScreen';
 import RegistrationScreen from '../screens/Authentication/RegistrationScreen';
 import ResetPasswordScreen from '../screens/Authentication/ResetPasswordScreen';
 import SetNewPasswordScreen from '../screens/Authentication/SetNewPasswordScreen';
