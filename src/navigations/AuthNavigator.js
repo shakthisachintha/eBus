1cb04fc49d863e6062a7d4ccc9e24011a3b6600c
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-07-04 14:21:28 +0530

    shakthi

---
+++ src/navigations/AuthNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { createStackNavigator } from "@react-navigation/stack"
+
+
+import DashboardNavigator from './DashboardNavigator';
+import LoginScreen from "../screens/Authentication/LoginScreen";
+import LoadingScreen from '../screens/loadingScreen';
+import RegistrationScreen from '../screens/Authentication/RegistrationScreen';
+import ResetPasswordScreen from '../screens/Authentication/ResetPasswordScreen';
+import SetNewPasswordScreen from '../screens/Authentication/SetNewPasswordScreen';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => (
+
+    <Stack.Navigator>
+
+        <Stack.Screen
+            name="Loading"
+            component={LoadingScreen}
+            options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{
+                headerShown: false,
+                headerLeft: null
+            }}
+        />
+
+        <Stack.Screen
+            name="Register"
+            component={RegistrationScreen}
+            options={{ title: "Register" }}
+        />
+
+        <Stack.Screen
+            name="ResetPassword"
+            component={ResetPasswordScreen}
+            options={{ title: "Forgot Password" }}
+        />
+
+        <Stack.Screen
+            name="Dashboard"
+            component={DashboardNavigator}
+            options={{ title: "Dashboard", headerShown: false }}
+        />
+
+        <Stack.Screen
+            name="LinkVerify"
+            options={{ title: "Reset Password" }}
+            component={SetNewPasswordScreen}
+        />
+
+    </Stack.Navigator>
+)
+
+export default AuthNavigator;
