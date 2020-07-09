0d77cd6bfe76dc1dcfff739948e9f47b4fb2845c
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-07-09 13:30:19 +0530

    integration of chnage password with backend

--- src/api/user.js
+++ src/api/user.js
@@ -2,11 +2,13 @@ import client from "./client";
 
 const register = (userInfo) => client.post("/user/register", userInfo);
 const updateDetails = (userInfo) => client.post("/user/update", userInfo);
+const updatePassword = (userInfo) => client.post("/user/changepassword", userInfo);
 
 const facebookRegister = (userInfo) => client.post("/user/register/facebook", userInfo);
 
 export default {
     register,
     facebookRegister,
-    updateDetails
+    updateDetails,
+    updatePassword
 }
