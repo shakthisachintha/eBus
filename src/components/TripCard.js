37dbe4c30600852a28172145df13b4827b61b5c3
Shakthi Sachintha <shakthisachintha@gmail.com>
2020-10-03 19:25:27 +0530

    MINOR CHANGES

---
+++ src/components/TripCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TripCard = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default TripCard
+
+const styles = StyleSheet.create({})
