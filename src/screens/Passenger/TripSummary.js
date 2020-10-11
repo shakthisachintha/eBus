64a1439e301bf5534a20bafb9378af4d6b90ce4d
Shakthi Sachintha <shakthisachintha@gmail.com>
2020-10-11 23:47:02 +0530

    Trip summary screen

---
+++ src/screens/Passenger/TripSummary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TripSummary = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default TripSummary
+
+const styles = StyleSheet.create({})
